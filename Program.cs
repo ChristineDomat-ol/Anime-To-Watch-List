203e872bfe59072f59d8698a5a5b693dc7604cbd
ChristineDomat-ol <christinedomatol@gmail.com>
2025-03-20 17:32:37 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿using System.Linq;
-    
+﻿
 namespace Anime_To_Watch_List
 {
+
     internal class Program
     {
+        static List<string> list = new List<string>();
+        static string[] actions = new string[] { "[1] Add Anime", "[2] Delete Anime", "[3] Mark as Watched", "[4] View Anime List", "[5] EXIT" };
+
         static void Main(string[] args)
         {
 
@@ -11,134 +14,173 @@ namespace Anime_To_Watch_List
 
             Console.WriteLine("Welcome to To-Watch Anime List");
 
-            List<string> list = new List<string>();
-            string transactionchoice;
-            do
+            ShowActions();
+            int action = GetUserInput();
+
+            while (action != 5)
             {
-                Console.WriteLine("\n[1] Add Anime\n[2] Delete Anime\n[3] Mark as Watched\n[4] View Anime List\n[5] EXIT");
-
-                Console.Write("\nACTIONS: ");
-                string action = Console.ReadLine();
-
-                if (action == "1")
+                if (action == 1)
                 {
-                    string addchoice;
-
-                    do
-                    {
-                        Console.Write("\nAdd Anime: ");
-                        string addanime = Console.ReadLine();
-
-                        if (list.Contains(addanime))
-                        {
-                            Console.WriteLine(addanime + " is already in the List");
-
-                        }
-
-                        else
-                        {
-                            list.Add(addanime);
-                            Console.WriteLine(addanime + " Added");
-                        }
-
-                        do
-                        {
-                            Console.Write("\n[Y/N] Add again?: ");
-                            addchoice = Console.ReadLine().ToLower();
-                        } while (addchoice != "y" && addchoice != "n");
-
-                        
-
-                    } while (addchoice == "y");
-
-
+                    AddorDelAnime(1);
                 }
-                else if (action == "2")
+                else if (action == 2)
                 {
-                    string delchoice;
-                    do
-                    {
-                        Console.Write("\nDelete Anime: ");
-                        string delanime = Console.ReadLine();
-                        if (list.Contains(delanime))
-                        {
-                            list.Remove(delanime);
-                            Console.WriteLine(delanime + " Deleted");
-                        }
-                        else
-                        {
-                            Console.WriteLine("\n" + delanime + " is not in the list");
-                        }
-
-                        do
-                        {
-                            Console.Write("\n[Y/N] Delete Again?: ");
-                            delchoice = Console.ReadLine().ToLower();
-                        } while (delchoice != "y" && delchoice != "n");
-
-                    } while (delchoice == "y");
+                    AddorDelAnime(2);
                 }
-                else if (action == "3")
+                else if (action == 3)
                 {
-                    string watchoice;
-                    do
-                    {
-                        Console.Write("\nMark as Watched: ");
-                        string watchanime = Console.ReadLine();
-                        if (list.Contains(watchanime))
-                        {
-                            list.Remove(watchanime);
-                            list.Add(watchanime + " - Watched");
-                            Console.WriteLine(watchanime + " has been Marked as Watched");
-                        }
-                        else
-                        {
-                            Console.WriteLine("\n" + watchanime + " is not in the list");
-                        }
-                        do
-                        {
-                            Console.Write("\n[Y/N] Mark as Watched Again?: ");
-                            watchoice = Console.ReadLine().ToLower();
-                        } while (watchoice != "y" && watchoice != "n");
-
-                    } while (watchoice == "y");
+                    MarkasWatched();
                 }
-                else if (action == "4")
+                else if (action == 4)
                 {
-                    if (list.Count == 0)
-                    {
-                        Console.WriteLine("List is Empty");
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nAnime List:");
-                        foreach (string i in list)
-                        {
-                            Console.WriteLine(i);
-                        }
-                    }
-
+                    View();
                 }
-                else if (action == "5")
+                else if (action == 5)
                 {
                     Console.WriteLine("\nThank you for using the Program\nDomat-ol, Christine L.\nBSIT 2-1");
                     Environment.Exit(0);
                 }
                 else
                 {
-                    Console.WriteLine("Invalid Input");
+                    Console.WriteLine("Invalid Input\n Enter 1-5");
                 }
+                ShowActions();
+                action = GetUserInput();
+            }
+            Console.WriteLine("\nThank you for using the Program\nDomat-ol, Christine L.\nBSIT 2-1");
+        }
+
+        static void ShowActions()
+        {
+            Console.WriteLine("\n-------------------");
+            Console.WriteLine("MENU");
+
+            foreach (string action in actions)
+            {
+                Console.WriteLine(action);
+            }
+        }
+
+        static int GetUserInput()
+        {
+            Console.Write("\nACTION: ");
+            int action = Convert.ToInt16(Console.ReadLine());
+            return action;
+        }
+
+        static void AddorDelAnime(int userAction)
+        {
+            if (userAction == 1)
+            {
                 do
                 {
-                    Console.Write("\n[Y/N] Do another transaction?: ");
-                    transactionchoice = Console.ReadLine().ToLower();
-                } while (transactionchoice != "y" && transactionchoice != "n");
+                    Console.Write("Enter Anime to Add: ");
+                    string useranimeinput = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(useranimeinput))
+                    {
+                        if (list.Contains(useranimeinput))
+                        {
+                            Console.WriteLine(useranimeinput + " is already in the List");
+                        }
 
-            } while (transactionchoice == "y");
+                        else
+                        {
+                            list.Add(useranimeinput);
+                            Console.WriteLine(useranimeinput + " Added");
+                            View();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please Enter Anime");
+                    }
+                } while (Again());
+            }
+            else if (userAction == 2)
+            {
+                do
+                {
+                    Console.Write("Enter Anime to Delete: ");
+                    string useranimeinput = Console.ReadLine();
 
-            Console.WriteLine("\nThank you for using the Program\nDomat-ol, Christine L.\nBSIT 2-1");
+                    if (!string.IsNullOrEmpty(useranimeinput))
+                    {
+                        if (list.Contains(useranimeinput))
+                        {
+                            list.Remove(useranimeinput);
+                            Console.WriteLine(useranimeinput + " Deleted");
+                            View();
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n" + useranimeinput + " is not in the list");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please Enter Anime");
+                    }
+                } while (Again());
+            }
+        }
 
+        static void MarkasWatched()
+        {
+            do
+            {
+                Console.Write("\nMark as Watched: ");
+                string watchanime = Console.ReadLine();
+                if (list.Contains(watchanime))
+                {
+                    list.Remove(watchanime);
+                    list.Add(watchanime + " - Watched");
+                    Console.WriteLine(watchanime + " has been Marked as Watched");
+                    View();
+                }
+                else
+                {
+                    Console.WriteLine("\n" + watchanime + " is not in the list");
+                }
 
+            }
+            while (Again());
+        }
+
+        static void View()
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("List is Empty");
+            }
+            else
+            {
+                Console.WriteLine("\nAnime List:");
+                foreach (string i in list)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static bool Again()
+        {
+            while (true)
+            {
+                Console.Write("\n[Y/N] Again? ");
+                string choice = Console.ReadLine().ToLower();
+
+                if (choice == "y")
+                {
+                    return true;
+                }
+                else if (choice == "n")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter Y/N");
+                }
+            }
         }
     }
 }
